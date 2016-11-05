11bbfc40926fa02c9030f7721ce3c3675c1ef235
Brandon <Brandon>
2016-11-04 21:06:49 -0400

    Semester Project ( Added Some Polling Data ) - 11.04.2016

--- Project/src/sql/tables.sql
+++ Project/src/sql/tables.sql
@@ -27,4 +27,20 @@ SELECT * FROM Stats_t;
 INSERT INTO Candidates_T(Candidate1, Candidate2, Candidate3) VALUES('Donald J. Trump','Hillary R. Clinton','Gary E. Johnson');
 
 /* Inset Information Into The Stats Table */
-INSERT INTO Stats_T(Can1Poll, Can2Poll, Can3Poll, DateOfRecord) VALUES('95%','-17%','8%',str_to_date('OCT,28,14 15,45','%b,%d,%Y %i,%s'));
+INSERT INTO Stats_T(Can1Poll, Can2Poll, Can3Poll, DateOfRecord) VALUES
+('43%','44%','6%',str_to_date('NOV,04,16 15,45','%b,%d,%Y %i,%s')),
+('42%','45%','5%',str_to_date('NOV,03,16 15,45','%b,%d,%Y %i,%s')),
+('44%','44%','4%',str_to_date('NOV,02,16 15,45','%b,%d,%Y %i,%s')),
+('46%','45%','3%',str_to_date('NOV,01,16 15,45','%b,%d,%Y %i,%s')),
+('47%','43%','No Data',str_to_date('OCT,31,16 15,45','%b,%d,%Y %i,%s')),
+('45%','46%','4%',str_to_date('OCT,30,16 15,45','%b,%d,%Y %i,%s')),
+('46%','44%','No Data',str_to_date('OCT,29,16 15,45','%b,%d,%Y %i,%s')),
+('46%','44%','No Data',str_to_date('OCT,28,16 15,45','%b,%d,%Y %i,%s')),
+('40%','46%','6%',str_to_date('OCT,27,16 15,45','%b,%d,%Y %i,%s'));
+
+/* Add More Data Here For Before October 27th, 2016 */
+
+SELECT * FROM STATS_T;
+
+DELETE FROM Stats_T
+	WHERE StatID = 4;
